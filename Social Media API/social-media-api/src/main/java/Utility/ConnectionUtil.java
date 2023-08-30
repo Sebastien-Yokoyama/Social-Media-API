c5286f279c41a5693c341ec63613517bdca38283
Sebastien-Yokoyama <syokoyama2001@gmail.com>
2023-08-29 20:42:42 -0700

    Created ConnectionUtil to manage RDBMS Connections

---
+++ Social Media API/social-media-api/src/main/java/Utility/ConnectionUtil.java
@@ -0,0 +1,28 @@
+// AUTHOR: Sebastien Yokoyama
+// FILE NAME: ConnectionUtil.java
+/* FILE DESCRIPTION: Class that connects to database via JDBC
+*/
+
+package Utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtil {
+    private static String url = "jdbc:sqlite:D:\\Programs\\SQLite/Databases\\SocialMediaApi\\SocialMediaDB.db";
+    private static Connection connection = null;
+
+    public static Connection getConnection(){
+        if(connection == null){
+            try{
+                connection = DriverManager.getConnection(url);
+            }
+            catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+}
--- Social Media API/social-media-api/src/main/java/syokoyama/App.java
+++ Social Media API/social-media-api/src/main/java/syokoyama/App.java
@@ -1,13 +1,32 @@
+// AUTHOR: Sebastien Yokoyama
+// FILE NAME: App.java
+/* FILE DESCRIPTION: Contains the main program
+*/
+
 package syokoyama;
 
-/**
- * Hello world!
- *
- */
+import Model.*;
+import Utility.ConnectionUtil;
+
+import java.sql.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Connection connection = ConnectionUtil.getConnection();
+
+        try{
+            String sql = "SELECT * FROM accounts";
+        
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            System.out.println("Acc ID: " + Integer.toString(rs.getInt("accountId")));
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
